2b525b56610e01a6b71c4108d4faca7a2eeb76ff
Elena Karlsen-Ayala <ekarlsenayala@login1.ufhpc>
2020-05-15 15:48:28 -0400

    Add a little data

--- sys_info_page2.sh
+++ sys_info_page2.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+# Program to output a system information pageecho "<html>
+<head>
+<title>System Information Report</title>
+</head>
+<body>
+<h1>System Information Report</h1>
+</body>
+</html>"
+
 <html>
 <head>
 <title>Page Title</title>
