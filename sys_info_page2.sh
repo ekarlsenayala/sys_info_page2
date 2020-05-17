044d400ebb18e360c3b3800e7145cd13b6390cf0
Elena Karlsen-Ayala <ekarlsenayala@login4.ufhpc>
2020-05-17 13:44:50 -0400

    change to cat from echo

--- sys_info_page2.sh
+++ sys_info_page2.sh
@@ -2,23 +2,22 @@
 # Program to output a system information pageecho
 #if you create the title= then it will change throughout the script 
 
-title="Systems Information Report
-echo  "<html>
-<head>
-<title>$title </title>
-</head>
-<body>
-<h1>$title</h1>
+TITLE="Systems Information Report for $HOSTNAME"
+CURRENT_TIME="$(date + "%x %r %z")"
+TIMESTAMP="Generated $CURRENT_TIME, by $USER"
+
+
+cat << _EOF_
+  "<html>
+ <head>
+ <title>$title </title>
+ </head>
+ <body>
+ <h1>$title</h1>
+<h1>$TITLE</h1>
+<p>$TIMESTAMP</p>
 </body>
-</html>"
+ </html>"
+_EOF_
 
 
-
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-Page body.
-</body>
-</html>
